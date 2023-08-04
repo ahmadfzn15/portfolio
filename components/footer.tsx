45fc508abaadaddc56b0476d9505f2df7fe5897a
ahmad fauzan <ahmadfzn2005@gmail.com>
2023-08-04 19:56:35 +0700

    new commit

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,9 +3,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <>
-      <div className="w-full p-10 absolute bottom-0">
-        <h1>Copyright © {Date}</h1>
-      </div>
+      <div className="w-full p-10 absolute bottom-0"></div>
     </>
   );
 }
